a52cdd18b7e8c2f55a5195f31e051e23984f2799
diegochiquero <chiquerodiego@gmail.com>
2022-02-20 14:52:41 +0100

    docs: some changes

--- sendemail.php
+++ sendemail.php
@@ -13,7 +13,6 @@ Nombre: '.$name.'
 Telefono: '.$telefono.'
 Texto: '.$texto.'
 ';
-// info@instamobel.com
-mail('livelife7102@yahoo.com', 'Formulario de contacto instamobel', utf8_decode($texto), 'From: ' . utf8_decode($name) . ' <' . $email . '>');
+mail('info@instamobel.com', 'Formulario de contacto instamobel', utf8_decode($texto), 'From: ' . utf8_decode($name) . ' <' . $email . '>');
 header('Location: correct_page.htm');
 ?>
